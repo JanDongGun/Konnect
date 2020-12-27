ec4f40cad168c09b7f16676c9158c991dae4a006
hoanganhquang <anhquang976@gmail.com>
2020-12-27 12:23:27 +0700

    setup page login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,13 +6,120 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _showPass = false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        "Hay ",
-        style: TextStyle(fontFamily: "Roboto", fontSize: 50),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: loginPageUI(),
       ),
     );
   }
+
+  Widget loginPageUI() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(15, 20, 15, 0),
+      child: Column(
+        children: [titleLoginWidget(), inputForm()],
+      ),
+    );
+  }
+
+  Widget titleLoginWidget() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(6, 50, 80, 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Let's Sign you In",
+            style: TextStyle(
+                fontFamily: "Roboto",
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.w900),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Welcome Back you have been missed!",
+            style: TextStyle(
+              fontFamily: "Roboto",
+              fontSize: 25,
+              color: Colors.white54,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget inputForm() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+      child: Column(
+        children: [
+          emailInputWidget(),
+          SizedBox(
+            height: 7,
+          ),
+          passwordInputWidget()
+        ],
+      ),
+    );
+  }
+
+  Widget emailInputWidget() {
+    return TextFormField(
+      style: TextStyle(color: Colors.white, fontSize: 15),
+      decoration: InputDecoration(
+        fillColor: Colors.grey[900],
+        filled: true,
+        hintStyle: TextStyle(color: Colors.white38, fontSize: 15),
+        hintText: "Email Address",
+        contentPadding: EdgeInsets.all(20),
+        focusedBorder:
+            OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+                color: Colors.grey, style: BorderStyle.solid, width: 2)),
+      ),
+    );
+  }
+
+  Widget passwordInputWidget() {
+    return TextFormField(
+      style: TextStyle(color: Colors.white, fontSize: 15),
+      obscureText: !_showPass,
+      decoration: InputDecoration(
+        fillColor: Colors.grey[900],
+        filled: true,
+        hintStyle: TextStyle(color: Colors.white38),
+        hintText: "Password",
+        contentPadding: EdgeInsets.all(20),
+        focusedBorder:
+            OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+                color: Colors.grey, style: BorderStyle.solid, width: 2)),
+        suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() {
+              _showPass = !_showPass;
+            });
+          },
+          child: Icon(
+            Icons.vpn_key,
+            color: Colors.white38,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget signinButtonWidget() {}
 }
