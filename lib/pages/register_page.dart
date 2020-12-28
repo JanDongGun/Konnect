1019d21ec2cfe77c784ad5a794c700e114df0a3d
dungpk14789 <62883445+dungpk14789@users.noreply.github.com>
2020-12-28 16:03:14 +0700

    update regis_page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -22,6 +22,10 @@ class _RegisPageState extends State<RegisPage> {
 
   File _image;
 
+  String _name;
+  String _email;
+  String _password;
+
   _RegisPageState() {
     _formKey = GlobalKey<FormState>();
   }
@@ -188,6 +192,16 @@ class _RegisPageState extends State<RegisPage> {
 
   Widget emailTextFieldWidget() {
     return TextFormField(
+      validator: (_input) {
+        return _input.length != 0 && _input.contains("@")
+            ? null
+            : "Please enter avalid email";
+      },
+      onSaved: (_input) {
+        setState(() {
+          _email = _input;
+        });
+      },
       keyboardType: TextInputType.emailAddress,
       style: TextStyle(color: Colors.white, fontSize: 15),
       decoration: InputDecoration(
@@ -208,6 +222,14 @@ class _RegisPageState extends State<RegisPage> {
 
   Widget nameTextFieldWidget() {
     return TextFormField(
+      validator: (_input) {
+        return _input.length != 0 ? null : "Please enter a name";
+      },
+      onSaved: (_input) {
+        setState(() {
+          _name = _input;
+        });
+      },
       style: TextStyle(color: Colors.white, fontSize: 15),
       decoration: InputDecoration(
         fillColor: Colors.grey[900],
@@ -235,6 +257,14 @@ class _RegisPageState extends State<RegisPage> {
         children: [
           Expanded(
             child: TextFormField(
+              validator: (_input) {
+                return _input.length != 0 ? null : "Please enter a password";
+              },
+              onSaved: (_input) {
+                setState(() {
+                  _password = _input;
+                });
+              },
               keyboardType: TextInputType.text,
               style: TextStyle(color: Colors.white, fontSize: 15),
               obscureText: _hidePass,
