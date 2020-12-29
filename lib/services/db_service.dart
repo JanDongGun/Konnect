ef0954dc3cc5f0fa3064786b468898dd0679fbe3
dungpk14789 <62883445+dungpk14789@users.noreply.github.com>
2020-12-29 11:46:18 +0700

    add db_service

--- lib/services/db_service.dart
+++ lib/services/db_service.dart
@@ -1,1 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DBService {
+  static DBService instance = DBService();
+
+  FirebaseFirestore _db;
+
+  DBService() {
+    _db = FirebaseFirestore.instance;
+  }
+
+  String _userCollection = "User";
+
+  Future<void> createUserInDB(
+      String _uid, String _name, String _email, String _imageURL) async {
+    try {
+      await _db.collection(_userCollection).doc(_uid).set({
+        "name": _name,
+        "email": _email,
+        "image": _imageURL,
+        "lastSeen": DateTime.now().toUtc()
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
