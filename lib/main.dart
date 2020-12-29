e8631cd7b77e5bc78c3b224cb442a43f2f62654a
dungpk14789 <62883445+dungpk14789@users.noreply.github.com>
2020-12-29 11:49:23 +0700

    import db_service

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:konnect/pages/login_page.dart';
 import 'package:konnect/pages/register_page.dart';
 import 'services/navigation_service.dart';
 import 'package:konnect/pages/started_page.dart';
+import 'package:konnect/services/db_service.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +16,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //DBService.instance.createUserInDB(
+    //   "0123", "Dung", "dung@gmail.com", "http://www.pravat.cc");
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       navigatorKey: NavigationService.instance.navigatorKey,
