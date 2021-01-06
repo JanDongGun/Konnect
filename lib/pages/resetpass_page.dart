b4a563a1d8858b2160960a2a4a6cf955b3b1cad6
hoanganhquang <anhquang976@gmail.com>
2021-01-06 12:04:20 +0700

    add resetpass page

---
+++ lib/pages/resetpass_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResetPassPage extends StatefulWidget {
+  @override
+  _ResetPassPageState createState() => _ResetPassPageState();
+}
+
+class _ResetPassPageState extends State<ResetPassPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(""),
+    );
+  }
+}
